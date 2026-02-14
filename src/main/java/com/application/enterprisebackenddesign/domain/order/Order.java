62f412d15a154a5e2a59f1061fad74ebaf4da3eb
ANNA\annav <annavidali99@gmail.com>
2026-02-14 17:15:18 +0100

    added addLine method to add a product to an order only if the order hasn’t been confirmed or cancelled

--- src/main/java/com/application/enterprisebackenddesign/domain/order/Order.java
+++ src/main/java/com/application/enterprisebackenddesign/domain/order/Order.java
@@ -1,4 +1,42 @@
 package com.application.enterprisebackenddesign.domain.order;
 
+import com.application.enterprisebackenddesign.domain.shared.DomainException;
+import com.application.enterprisebackenddesign.domain.shared.Money;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class Order {
+    private final Long id;
+    private final Long customerId;
+    private OrderStatus status;
+    private final List<OrderLine> orderLines;
+    private Money totalAmount;
+
+    public Order(Long id, Long customerId, List<OrderLine> orderLines) {
+        this.id = id;
+        this.customerId = customerId;
+        this.status = OrderStatus.CREATED;
+        this.orderLines = orderLines;
+        this.totalAmount = calculateTotal();
+    }
+
+    public void addLine(OrderLine orderLine) throws DomainException.BusinessRuleViolationException {
+
+        if(orderLine == null){
+            throw new DomainException.BusinessRuleViolationException("Order line cannot be null.");
+        }
+
+        if(orderLines.contains(orderLine)){
+            throw new DomainException.BusinessRuleViolationException("Order line already exists.");
+        }
+
+        if(status == OrderStatus.CREATED) {
+            orderLines.add(orderLine);
+            totalAmount = calculateTotal();
+        } else {
+            throw new DomainException.BusinessRuleViolationException("Cannot add line to order with status: " + status);
+        }
+    }
 }
