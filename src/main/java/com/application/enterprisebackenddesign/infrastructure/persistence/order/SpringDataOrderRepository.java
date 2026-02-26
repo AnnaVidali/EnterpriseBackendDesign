1dab9648348f030a7f838752e9a4b8ec9141a417
ANNA\annav <annavidali99@gmail.com>
2026-02-27 00:00:03 +0100

    removed unused import

--- src/main/java/com/application/enterprisebackenddesign/infrastructure/persistence/order/SpringDataOrderRepository.java
+++ src/main/java/com/application/enterprisebackenddesign/infrastructure/persistence/order/SpringDataOrderRepository.java
@@ -1,6 +1,5 @@
 package com.application.enterprisebackenddesign.infrastructure.persistence.order;
 
-import com.application.enterprisebackenddesign.domain.order.OrderStatus;
 import com.application.enterprisebackenddesign.infrastructure.persistence.order.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
