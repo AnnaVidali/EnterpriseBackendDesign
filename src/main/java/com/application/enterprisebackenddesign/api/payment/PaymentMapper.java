fa4480fd5b6115ece5515cd84665b1bc0e5aed11
ANNA\annav <annavidali99@gmail.com>
2026-05-03 18:13:53 +0200

    implement of api.payment with controller, mapper, request and response

---
+++ src/main/java/com/application/enterprisebackenddesign/api/payment/PaymentMapper.java
@@ -0,0 +1,28 @@
+package com.application.enterprisebackenddesign.api.payment;
+
+import com.application.enterprisebackenddesign.domain.payment.Payment;
+import com.application.enterprisebackenddesign.domain.shared.Money;
+import org.springframework.stereotype.Component;
+
+import java.util.Currency;
+
+@Component
+public class PaymentMapper {
+
+    public PaymentResponse toResponse(Payment payment) {
+        return new PaymentResponse(
+                payment.getId(),
+                payment.getInvoiceId(),
+                payment.getOrderId(),
+                payment.getCustomerId(),
+                payment.getAmount().getAmount(),
+                payment.getAmount().getCurrency().getCurrencyCode(),
+                payment.getStatus().name(),
+                payment.getPaymentDate()
+        );
+    }
+
+    public Money toMoney(PaymentRequest request) {
+        return new Money(request.amount(), Currency.getInstance(request.currency()));
+    }
+}
