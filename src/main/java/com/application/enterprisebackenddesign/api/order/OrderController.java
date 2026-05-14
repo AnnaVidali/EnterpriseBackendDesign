65a8f5f5e3d6a83565e35150ec8316edf82338b6
ANNA\annav <annavidali99@gmail.com>
2026-05-15 01:12:54 +0200

    minor change

--- src/main/java/com/application/enterprisebackenddesign/api/order/OrderController.java
+++ src/main/java/com/application/enterprisebackenddesign/api/order/OrderController.java
@@ -72,7 +72,7 @@ public class OrderController {
 
     @PostMapping("/{id}/lines")
     public OrderResponse addLine(@PathVariable Long id, @RequestBody OrderLineRequest request) throws DomainException {
-        Currency currency = Currency.getInstance("EUR");
+        Currency currency = Currency.getInstance(request.currency());
         Money price = new Money(request.price(), currency);
         Order order = addOrderLineUseCase.execute(id, UUID.randomUUID().getMostSignificantBits(), request.productId(), price, request.quantity());
         return orderMapper.toResponse(order);
